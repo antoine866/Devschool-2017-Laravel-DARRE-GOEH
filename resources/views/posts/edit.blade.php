e6f75ee5a39b9d18e75f3424bb471cecf2deb640
Antoine <antoine.darre@devinci.fr>
2016-12-05 15:53:02 +0100

    Initial Commit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,1 +1,42 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Editer un article</div>
+                    <div class="panel-body">
+
+                        {!! Form::model(
+                        $post,
+                        array(
+                        'route' => array('post.update', $post->id),
+                        'method' => 'PUT'))
+                        !!}
+
+                        {!! Form::label('title', 'Titre') !!}
+
+                        {!! Form::text('title', null,
+                        ['class' => 'form-control',
+                         'placeholder' => 'Titre']) !!}
+
+                        {!! Form::label('content', 'Contenu') !!}
+
+                        {!! Form::textarea('content', null,
+                        ['class' => 'form-control',
+                         'placeholder' => 'Contenu']) !!}
+
+                        <br>
+
+                        {!! Form::submit('Publier', ['class' => 'btn btn-info']) !!}
+
+                        {!! Form::close() !!}
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsectionphp artisan serve
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,1 +1,48 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">{{$post->title}}</div>
+                    <div class="panel-body">
+                        {{ $post->content }}
+
+                        <br>
+                        <br>
+                        <strong>Auteur : {{$post->user->name}}</strong>
+                        <br>
+
+                        @if(Auth::check() && Auth::user()->isAdmin)
+
+                            <a href="{{ route('post.edit', $post->id) }}">Modifier</a>
+
+                            <br>
+                            <br>
+
+                            {!! Form::model(
+                             $post,
+                             array(
+                             'route' => array('post.destroy', $post->id),
+                             'method' => 'DELETE'))
+                             !!}
+
+                            {!! Form::submit('Supprimer',
+                             ['class' => 'btn btn-danger'])
+                             !!}
+
+                            {!! Form::close() !!}
+
+                        @endif
+
+                        <br>
+
+                        <a href="{{ route('post.index') }}">Retour aux articles</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
